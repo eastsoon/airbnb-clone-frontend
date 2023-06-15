60ca3216a460a8393a04b235dfd3e8b58b6b7cad
EastSoon <jds920805@gmail.com>
2023-06-15 11:04:28 +0900

    20.10 Kakao Talk Auth

--- src/components/SocialLogin.tsx
+++ src/components/SocialLogin.tsx
@@ -2,6 +2,13 @@ import { Box, Button, Divider, HStack, Text, VStack } from "@chakra-ui/react";
 import { FaGithub, FaComment } from "react-icons/fa";
 
 export default function SocialLogin() {
+  const kakaoParams = {
+    client_id: "ed0047ab00386e970dd1c124810c7e4e",
+    redirect_uri: "http://127.0.0.1:3000/social/kakao",
+    response_type: "code",
+  };
+  const params = new URLSearchParams(kakaoParams).toString();
+  console.log(params);
   return (
     <Box mb={4}>
       <HStack my={8}>
@@ -21,7 +28,13 @@ export default function SocialLogin() {
         >
           Continue With GitHub
         </Button>
-        <Button w="100%" leftIcon={<FaComment />} colorScheme={"yellow"}>
+        <Button
+          as="a"
+          href={`https://kauth.kakao.com/oauth/authorize?${params}`}
+          w="100%"
+          leftIcon={<FaComment />}
+          colorScheme={"yellow"}
+        >
           Continue With Kakao
         </Button>
       </VStack>
--- src/routes/KakaoConfirm.tsx
+++ src/routes/KakaoConfirm.tsx
@@ -2,9 +2,9 @@ import { Heading, Spinner, Text, useToast, VStack } from "@chakra-ui/react";
 import { useQueryClient } from "@tanstack/react-query";
 import { useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
-import { githubLogIn } from "../api";
+import { kakaoLogIn } from "../api";
 
-export default function GithubConfirm() {
+export default function KakaoConfirm() {
   const toast = useToast();
   const queryClient = useQueryClient();
   const { search } = useLocation();
@@ -13,7 +13,7 @@ export default function GithubConfirm() {
     const params = new URLSearchParams(search);
     const code = params.get("code");
     if (code) {
-      const status = await githubLogIn(code);
+      const status = await kakaoLogIn(code);
       if (status === 200) {
         toast({
           status: "success",
